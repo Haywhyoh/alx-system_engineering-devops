f9420822de7492df5374b51e07c693a02d860cdc
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-17 13:06:39 +0100

    wrote the first part of task 1

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+gathers information about from api
+about an employee by ID and returns
+"""
+import requests
+import sys
+
+
+def get_info():
+    """the method to get info"""
+    name_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1])
+    name = requests.get(name_url).json().get('name')
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = tasks.json()
+    complete = 0
+    titles = []
+    total = 0
+    for task in tasks:
+        if task['userId'] == int(sys.argv[1]):
+            if task['completed'] is True:
+                complete += 1
+                titles.append(task['titles'])
+            total += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, complete, total))
+    for title in titles:
+        print('\t ', end="")
+        print(title)
+    
+if __name__ == "__main__":
+    get_info()
