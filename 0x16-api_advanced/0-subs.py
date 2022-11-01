cdde60c4de19b7c1c3e47af952bcf74d0959d238
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-11-01 13:43:50 +0100

    no redirection

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,16 +8,20 @@ import sys
 
 
 def number_of_subscribers(subreddit):
-    """get number of subscribers"""
-    user_agent = {'User-agent': 'Ayomide'}
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': u_agent
+    }
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    raw_data = requests.get(url, headers=user_agent, allow_redirects=False)
-    result = raw_data.json()
-    if raw_data.status_code != 200:
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
-    result = raw_data.json()
-    if 'data' not in result:
+    dic = res.json()
+    if 'data' not in dic:
         return 0
-    if 'subscribers' not in result.get('data'):
+    if 'subscribers' not in dic.get('data'):
         return 0
-    result(result['data']['subscribers'])
+    return res.json()['data']['subscribers']
